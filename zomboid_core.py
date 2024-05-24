b5359f4de7c0230367a2c4ddb7e3a80fbfd34740
Aaron <fallenseriphan@gmail.com>
2024-05-24 16:56:19 -0600

    fleshed out installer method and prepping for first test

--- zomboid_core.py
+++ zomboid_core.py
@@ -6,7 +6,7 @@ def cmd_line_args():
     parser = argparse.ArgumentParser(prog="zomboid_core.py", description="Basic tools available from the command line.")
     subparsers = parser.add_subparsers(dest="command")
     parser_install = subparsers.add_parser("install")
-    parser_install.add_argument("--install_target", choices=["lgsm", "sysd", "rcon"], default=None)
+    parser_install.add_argument("--install_target", choices=["lgsm", "sysd", "rcon"], default=None, dest="target")
     parser_backup = subparsers.add_parser("backup")
     parser_restart = subparsers.add_parser("restart")
     parser_restart.add_argument("--message")
@@ -20,17 +20,7 @@ def main():
 
     match args.command:
         case "install":
-                print("Hello, and welcome to the A Path Above computer aided lgsm + Utility installer.")
-                sleep(5)
-                print("Now beginning the lgsm install. Be prepared to answer questions as needed.")
-                installer("lgsm")
-                print("Thank you, lgsm install complete")
-                sleep(5)
-                print("deploying systemd files")
-                installer("sysd")
-                sleep(5)
-                print("Installing rcon.")
-                installer("rcon")
+            installer(args.target)
         case "backup": 
             print("backup code")
         case "restart":
@@ -39,25 +29,40 @@ def main():
             print("chunk code")
         case "banparse":
             print("banparse code")
-        case "?" | "-?" | "--help":
-            help()
         case _:
-            print("Unrecognized Switch.")
-            help()
+            print("temp")
 
 def installer(service):
+    print("Hello, and welcome to the A Path Above computer aided lgsm + Utility installer.")
+    sleep(5)
+    if service:
+        print(f"You've chosen to install the specific module: {service}")
     match service:
         case "lgsm":
-            print("lgsm")
-            #linux_files.create_lgsm_folder()
-            #linux_files.create_linuxgsm()
-            #linux_files.download_pzserver()
+            print("Installing lgsm")
+            linux_files.create_lgsm_folder()
+            linux_files.create_linuxgsm()
+            linux_files.download_pzserver()
+            print("lgsm install complete")
         case "sysd":
             print("sysd")
             #linux_files.create_sysd_folders()
             #linux_files.deploy_sysd_files()
         case "rcon":
             print("Download rcon")
+        case _:
+            print("Installing all modules now.")
+            linux_files.create_lgsm_folder()
+            linux_files.create_linuxgsm()
+            linux_files.download_pzserver()
+            print("lgsm install complete")
+            sleep(10)
+            print("Deploying systemd files")
+            linux_files.create_sysd_folders()
+            linux_files.deploy_sysd_files()
+            print("sysd files deployed and activated")
+            sleep(10)
+            print("Installing rcon.")
 
 if __name__ == '__main__':
     main()
