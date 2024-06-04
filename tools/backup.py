7ca4bc875c5b9c243aec73ef9a02b06967b772b4
Aaron <fallenseriphan@gmail.com>
2024-06-04 01:22:42 -0600

    Added H:M to backup file names so multiple can be taken.

--- tools/backup.py
+++ tools/backup.py
@@ -1,18 +1,19 @@
 import subprocess
 from subprocess import CalledProcessError
 import datetime
-from datetime import date
+from datetime import datetime, timedelta
+import pytz
 
 from tools.linux_files import LinuxFiles
 
-from utils.config import backupRetentionDays
+from utils.config import backupRetentionDays, dailyBackupTimeZone
 
 def backup_handler():
     backupPath = LinuxFiles.get_daily_backup_path()
     stagingPath = f"{backupPath}/staging/"
-    dateToDay = date.today()
-    today = dateToDay.strftime("%d_%m_%Y")
-    nDaysAgo = (dateToDay - datetime.timedelta(backupRetentionDays)).strftime("%d_%m_%Y")
+    dateToDay = datetime.now(pytz.timezone(dailyBackupTimeZone))
+    today = dateToDay.strftime("%d_%m_%Y-%H:%M")
+    nDaysAgo = (dateToDay - timedelta(days=backupRetentionDays)).strftime("%d_%m_%Y")
 
     try:
         subprocess.run(["rsync", "-aq", "--exclude", "\'backups\'" "--delete",
