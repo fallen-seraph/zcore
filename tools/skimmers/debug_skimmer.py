d7dd88b903886a945f28479997a3c611b60e78e4
Aaron <fallenseriphan@gmail.com>
2024-06-04 01:24:11 -0600

    moved logprocessing directly into debug skimmer for better thread control

--- tools/skimmers/debug_skimmer.py
+++ tools/skimmers/debug_skimmer.py
@@ -3,9 +3,9 @@ import threading
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-from tools.linux_files import LinuxFiles
+from tools import restart, linux_services, discord, lgsm
 
-from tools.skimmers import log_processing
+from tools.linux_files import LinuxFiles
 
 class LogHandler(FileSystemEventHandler):
     def __init__(self, directory, process_line_callback):
@@ -13,6 +13,7 @@ class LogHandler(FileSystemEventHandler):
         self.process_line_callback = process_line_callback
         self.current_file = None
         self.file_handle = None
+        self.lock = threading.Lock()
 
     def on_modified(self, event):
         if event.is_directory:
@@ -38,10 +39,30 @@ class LogHandler(FileSystemEventHandler):
             line = self.file_handle.readline() # type: ignore
             if not line:
                 break
-            self.process_line_callback(line)
+            self.process_line_callback(line, self.lock)
 
-def process_line(line):
-    threading.Thread(target=log_processing.debug_log, args=(line,)).start()
+def process_line(line, lock):
+    print("prematch: " + line)
+    match line.split(","):
+        case [*_, "Mods need update"]:
+            lock.acquire()
+            threading.Thread(target=restart.restart_handler, args=("a mod update", None, False, False)).start()
+            time.sleep(10)
+            lock.release()
+        case [*_, "Failed to connect to Steam servers"]:
+            print("Steam's down")
+            time.sleep(20)
+            discord.discord_player_notifications("Steam connection failure, "
+                "restarting.")
+            active = linux_services.get_service_status("zomboid_core.service")[1]
+            lock.acquire()
+            if active != "active":
+                threading.Thread(target=linux_services.core_service, args=("restart",)).start()
+            else:
+                threading.Thread(target=linux_services.core_service, args=("start",)).start()
+            time.sleep(10)
+            lock.release()
+    
 
 def monitor_directory():
     directory = LinuxFiles.get_zomboid_logs()
