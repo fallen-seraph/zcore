3fb3229d5244c3dd651546b2be6e8e18288ef020
Aaron <fallenseriphan@gmail.com>
2024-06-04 01:24:45 -0600

    Started writing crash report.

---
+++ tools/report.py
@@ -0,0 +1,21 @@
+from subprocess import run, PIPE, CalledProcessError
+from tools import discord
+
+def crash_report():
+    #Crash report generator
+    discordMessage = "The server is down."
+    discord.discord_admin_notifications(discordMessage)
+
+    try:
+        diskReport = run(["df", "-h", "/"], stdout=PIPE, text=True).stdout
+    except CalledProcessError as e:
+        print(f"Disk Report failed: {e}")
+
+    discord.discord_admin_notifications(diskReport)
+
+    try:
+        journalReport = run(["journalctl", "--user", "-qn20"], stdout=PIPE, text=True).stdout
+    except CalledProcessError as e:
+        print(f"Disk Report failed: {e}")
+
+    discord.discord_admin_notifications(journalReport)
