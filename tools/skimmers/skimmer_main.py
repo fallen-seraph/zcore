504dfb12c748dc8f4047188085181406634a0ccf
Aaron <fallenseriphan@gmail.com>
2024-08-29 00:17:14 -0600

    Started refactoring skimmer_main

--- tools/skimmers/skimmer_main.py
+++ tools/skimmers/skimmer_main.py
@@ -6,15 +6,18 @@ from tools.file_manager import ZCoreFiles
 
 def monitor_logs():
     zcoreFiles = ZCoreFiles()
-    event_handler = debug_skimmer.DebugLogHandler(zcoreFiles.zomboidLogs,
-        log_processing.debug_line_process, "_DebugLog-server.txt")
+
+    event_handler = debug_skimmer.DebugLogHandler(zcoreFiles.zomboidLogs, log_processing.debug_line_process, "_DebugLog-server.txt")
+
     observer = Observer()
     observer.schedule(event_handler, zcoreFiles.zomboidLogs, False)
     observer.start()
+
     try:
         while True:
             time.sleep(1)
     except (KeyboardInterrupt, SystemExit):
         observer.stop()
         zcoreFiles.clear_process_tracker()
+        
     observer.join()
