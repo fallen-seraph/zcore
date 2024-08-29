c63ac6e2823adec67f4a778606583ecfd91069bd
Aaron <fallenseriphan@gmail.com>
2024-08-29 02:20:33 -0600

    moved each command into it's own function for readability.

--- zomboid_core.py
+++ zomboid_core.py
@@ -14,90 +14,100 @@ from tools import (
 
 from tools.skimmers import skimmer_main
 
-def main():
-    args = arguments.CMD_line_args()
-    active = linux_services.get_service_status(
-        "zomboid_core.service")[1]
 
+args = arguments.CMD_line_args()
+active = linux_services.get_service_status(
+    "zomboid_core.service")[1]
+
+def main():
     match args.command:
         case "install":
             install_mode(args.target)
         case "backup": 
-            if active != "active" or args.backupForce:
-                backup.backup_handler()
-            else:
-                print("Server is online. Shutdown before backing up or force backup")
+            parse_backup_arguments()
         case "restart":
-            if args.scheduled:
-                scheduler.restart_scheduler()
-            elif args.cancel:
-                utilities.cancel_pending_restart(args.cancel)
-            elif args.instant:
-                linux_services.core_service("restart")
-            else:
-                restart.restart_server_with_messages(
-                    args.message,
-                    args.delay,
-                    args.backup,
-                    args.stop
-                )
+            parse_restart_arguments()
         case "chunk":
-            if active != "active" or args.force:                
-                if not args.range:
-                    chunks.delete_chunks_from_file(args.file)
-                elif args.file_name:
-                    chunks.create_chunk_list_file(
-                        args.chunk_one,
-                        args.chunk_two,
-                        args.file_name
-                    )
-                else:
-                    chunks.delete_chunks_from_given_range(
-                        args.chunk_one,
-                        args.chunk_two
-                    )
-            else:
-                print("Warning: Deleting Files - Shutdown server or force "
-                    "with --force.")
+            parse_chunk_arguments()
         case "ban":
             ban.console_ban_handler(args.file)
         case _:
-            if args.skimmer:
-                skimmer_main.monitor_logs()
-            elif args.report:
-                report.crash_report()
-            else:
-                print("empty")
+            parse_misc_or_no_arguments()
 
 def install_mode(service):
-    print("Hello, and welcome to the A Path Above computer aided "
-        "lgsm + Utility installer.")
+    print("Starting install mode")
     time.sleep(1)
     if service:
-        print("You've chosen to install the specific module: "
-            f"{service}")
+        print(f"installing {service} service")
     linuxInstaller = linux_installer.Installer()
+    linuxInstallerFunctions = [
+        linuxInstaller.deploy_lgsm,
+        linuxInstaller.deploy_sysd,
+        linuxInstaller.misc_tasks
+    ]
     match service:
         case "lgsm":
             print("Installing lgsm, this will take a moment.")
-            linuxInstaller.deploy_lgsm()
+            linuxInstallerFunctions[0]()
             print("lgsm install complete")
         case "sysd":
-            linuxInstaller.deploy_sysd()
+            linuxInstallerFunctions[1]()
         case "misc":
-            linuxInstaller.misc_tasks()
+            linuxInstallerFunctions[2]()
         case _:
             print("Installing all modules now.")
-            linuxInstaller.deploy_lgsm()
-            print("lgsm install complete")
-            time.sleep(10)
-            print("Deploying systemd files")
-            linuxInstaller.deploy_sysd()
-            print("sysd files deployed and activated")
-            time.sleep(10)
-            print("finalizing tasks")
-            linuxInstaller.misc_tasks()
-            print("install complete")
+            for installer in linuxInstallerFunctions:
+                installer()
+
+def parse_backup_arguments():
+    if active != "active" or args.backupForce:
+        thread = backup.backup_handler()
+        if thread:
+            thread.join()
+    else:
+        print("Server is online. Shutdown before backing up or force backup")
+
+def parse_restart_arguments():
+    if args.scheduled:
+        scheduler.restart_scheduler()
+    elif args.cancel:
+        utilities.cancel_pending_restart(args.cancel)
+    elif args.instant:
+        linux_services.core_service("restart")
+    else:
+        restart.restart_server_with_messages(
+            args.message,
+            args.delay,
+            args.backup,
+            args.stop
+        )
+
+def parse_chunk_arguments():
+    if active != "active" or args.force:                
+        if not args.range:
+            chunks.delete_chunks_from_file(args.file)
+        elif args.file_name:
+            chunks.create_chunk_list_file(
+                args.chunk_one,
+                args.chunk_two,
+                args.file_name
+            )
+        else:
+            chunks.delete_chunks_from_given_range(
+                args.chunk_one,
+                args.chunk_two
+            )
+    else:
+        print("Warning: Deleting Files - Shutdown server or force "
+            "with --force.")
+        
+def parse_misc_or_no_arguments():
+    if args.skimmer:
+        skimmer_main.monitor_logs()
+    elif args.report:
+        report.crash_report()
+    else:
+        print("empty")
 
 if __name__ == '__main__':
     main()
